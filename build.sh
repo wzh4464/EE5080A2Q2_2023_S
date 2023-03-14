9d0eddff4acc400eef30671b635471430fcb640e
Hance Wu <32484940+wzh4464@users.noreply.github.com>
2023-03-14 16:14:07 +0800

    Q2

--- build.sh
+++ build.sh
@@ -2,4 +2,14 @@
 #   export LDFLAGS="-L/opt/homebrew/opt/lapack/lib"
 #   export CPPFLAGS="-I/opt/homebrew/opt/lapack/include"
 
-g++ -std=c++11 -o main main.cpp -llapacke -llapack -lblas -L/opt/homebrew/opt/lapack/lib -I/opt/homebrew/opt/lapack/include
+# add_definitions(-DGL_SILENCE_DEPRECATION)
+
+# include_directories(/usr/local/include)
+# include_directories(/opt/homebrew/include)
+
+# "-framework glut"
+# "-framework opengl"
+# "-lglew"
+# "debug mode"
+
+g++ -std=c++11 -o main main.cpp -llapacke -llapack -lblas -L/opt/homebrew/opt/lapack/lib -I/opt/homebrew/opt/lapack/include -I/opt/homebrew/include -I/usr/local/include -DGL_SILENCE_DEPRECATION -framework glut -framework opengl -lglew -g
--- main.cpp
+++ main.cpp
@@ -7,14 +7,22 @@
 #include <stdlib.h>
 #include <math.h>
 #include <lapacke.h>
+#include <GL/glew.h>
+#include <GLUT/glut.h>
 
-int main(){
+int main(int argc, char **argv)
+{
+    // init glut and glew
+    glutInit(&argc, argv);
+    // glewExperimental = GL_TRUE;
+    // glewInit();
+
     // vector vrp = (0,100,-100)
-    double vrp[3] = {0,100,-100};
+    double vrp[3] = {0, 100, -100};
     // vector vpn = (0,100,-100)
-    double vpn[3] = {0,100,-100};
+    double vpn[3] = {0, 100, -100};
     // vector vup = (1,0,0)
-    double vup[3] = {1,0,0};
+    double vup[3] = {1, 0, 0};
     // vector zvc is normalized vpn
     double zvc[3];
     double vpn_norm = 0;
@@ -22,57 +30,106 @@ int main(){
     vpn_norm = LAPACKE_dlange(LAPACK_ROW_MAJOR, 'F', 1, 3, zvc, 3);
     // printf("vpn_norm = %f\n",vpn_norm);
     // normalize vpn
-    LAPACKE_dlascl(LAPACK_ROW_MAJOR, 'G', 0, 0, 1.0, 1/vpn_norm, 1, 3, zvc, 3);
-    printf("zvc = (%f,%f,%f)\n",zvc[0],zvc[1],zvc[2]);
+    LAPACKE_dlascl(LAPACK_ROW_MAJOR, 'G', 0, 0, 1.0, 1 / vpn_norm, 1, 3, zvc, 3);
+    printf("zvc = (%f,%f,%f)\n", zvc[0], zvc[1], zvc[2]);
 
     // vector xvc is normalized vup x vpn
     double xvc[3];
     double vup_x_vpn[3];
-    vup_x_vpn[0] = vup[1]*vpn[2] - vup[2]*vpn[1];
-    vup_x_vpn[1] = vup[2]*vpn[0] - vup[0]*vpn[2];
-    vup_x_vpn[2] = vup[0]*vpn[1] - vup[1]*vpn[0];
+    vup_x_vpn[0] = vup[1] * vpn[2] - vup[2] * vpn[1];
+    vup_x_vpn[1] = vup[2] * vpn[0] - vup[0] * vpn[2];
+    vup_x_vpn[2] = vup[0] * vpn[1] - vup[1] * vpn[0];
     double vup_x_vpn_norm = 0;
     LAPACKE_dlacpy(LAPACK_ROW_MAJOR, 'A', 1, 3, vup_x_vpn, 3, xvc, 3);
     vup_x_vpn_norm = LAPACKE_dlange(LAPACK_ROW_MAJOR, 'F', 1, 3, xvc, 3);
     // printf("vup_x_vpn_norm = %f\n",vup_x_vpn_norm);
     // normalize vup x vpn
-    LAPACKE_dlascl(LAPACK_ROW_MAJOR, 'G', 0, 0, 1.0, 1/vup_x_vpn_norm, 1, 3, xvc, 3);
-    printf("xvc = (%f,%f,%f)\n",xvc[0],xvc[1],xvc[2]);
+    LAPACKE_dlascl(LAPACK_ROW_MAJOR, 'G', 0, 0, 1.0, 1 / vup_x_vpn_norm, 1, 3, xvc, 3);
+    printf("xvc = (%f,%f,%f)\n", xvc[0], xvc[1], xvc[2]);
 
     // vector yvc is zvc x xvc
     double yvc[3];
-    yvc[0] = zvc[1]*xvc[2] - zvc[2]*xvc[1];
-    yvc[1] = zvc[2]*xvc[0] - zvc[0]*xvc[2];
-    yvc[2] = zvc[0]*xvc[1] - zvc[1]*xvc[0];
-    printf("yvc = (%f,%f,%f)\n",yvc[0],yvc[1],yvc[2]);
+    yvc[0] = zvc[1] * xvc[2] - zvc[2] * xvc[1];
+    yvc[1] = zvc[2] * xvc[0] - zvc[0] * xvc[2];
+    yvc[2] = zvc[0] * xvc[1] - zvc[1] * xvc[0];
+    printf("yvc = (%f,%f,%f)\n", yvc[0], yvc[1], yvc[2]);
 
     // M_wc_from_vc
     double M_wc_from_vc[4][4] = {
-        {xvc[0],yvc[0],zvc[0],vrp[0]},
-        {xvc[1],yvc[1],zvc[1],vrp[1]},
-        {xvc[2],yvc[2],zvc[2],vrp[2]},
-        {0,0,0,1}
-    };
+        {xvc[0], yvc[0], zvc[0], vrp[0]},
+        {xvc[1], yvc[1], zvc[1], vrp[1]},
+        {xvc[2], yvc[2], zvc[2], vrp[2]},
+        {0, 0, 0, 1}};
     printf("M_wc_from_vc = \n");
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            printf("%f ",M_wc_from_vc[i][j]);
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%f ", M_wc_from_vc[i][j]);
         }
         printf("\n");
     }
 
     // M_vc_from_wc
     double M_vc_from_wc[4][4] = {
-        {xvc[0],xvc[1],xvc[2],-vrp[0]*xvc[0]-vrp[1]*xvc[1]-vrp[2]*xvc[2]},
-        {yvc[0],yvc[1],yvc[2],-vrp[0]*yvc[0]-vrp[1]*yvc[1]-vrp[2]*yvc[2]},
-        {zvc[0],zvc[1],zvc[2],-vrp[0]*zvc[0]-vrp[1]*zvc[1]-vrp[2]*zvc[2]},
-        {0,0,0,1}
-    };
+        {xvc[0], xvc[1], xvc[2], -vrp[0] * xvc[0] - vrp[1] * xvc[1] - vrp[2] * xvc[2]},
+        {yvc[0], yvc[1], yvc[2], -vrp[0] * yvc[0] - vrp[1] * yvc[1] - vrp[2] * yvc[2]},
+        {zvc[0], zvc[1], zvc[2], -vrp[0] * zvc[0] - vrp[1] * zvc[1] - vrp[2] * zvc[2]},
+        {0, 0, 0, 1}};
 
     printf("M_vc_from_wc = \n");
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            printf("%f ",M_vc_from_wc[i][j]);
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%f ", M_vc_from_wc[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Window Title");
+
+    // (b)
+    // define cavalier projection matrix
+    // projection vector is (1,1,sqrt(2))
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // show the matrix （row major）
+    // note that N is saved in column major
+    double N[16];
+    glGetDoublev(GL_PROJECTION_MATRIX, N);
+    printf("N = \n");
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%f ", N[j * 4 + i]);
+        }
+        printf("\n");
+    }
+    
+
+
+    // (c)
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(30, 0, -100);
+    glRotatef(60, 0, 1, 0);
+
+    // show the matrix （row major）
+    // note that M is saved in column major
+    double M[16];
+    glGetDoublev(GL_MODELVIEW_MATRIX, M);
+    printf("M = \n");
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%f ", M[j * 4 + i]);
         }
         printf("\n");
     }
